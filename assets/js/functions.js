07446ccac20f1c975d260930ba58ca8bf10b9aab
Yan Zinchenko <zinchenko.yna@gmail.com>
2015-10-08 00:16:01 +0300

    hah

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
 $(function() {
   smoothScroll(1000);
-//  workBelt();
+
 });
 // smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
@@ -26,21 +26,3 @@ function smoothScroll (duration) {
 }
 
 
-//function workBelt() {
-//
-//  $('.thumb-unit').click(function() {
-//    $('.work-belt').css('left','-100%');
-//    $('.work-container').show();
-//  });
-//  
-//  $('.work-return').click(function(){
-//    
-//  $('.work-belt').css('left','0%');
-//  $('.work-container').hide(800);
-//    
-//    
-//  });
-//
-//
-//
-//}
