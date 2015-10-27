86e4f9d72d9b5df226a339c94108caddc6938263
Yan Zinchenko <zinchenko.yna@gmail.com>
2015-10-27 14:36:49 +0200

    Keep on hacking!!

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -5,7 +5,7 @@ $(function() {
   workBelt();
   workLoad();
   teamStuff();
-  
+  $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
   
 });
 // smoothScroll function is applied from the document ready function
@@ -111,3 +111,36 @@ function teamStuff() {
   });
   
 }
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
